d54f398b6e635c79c188c7d97934343d43a3b9dd
Kavindu Chamith <kavindu.chamith@pagero.com>
2022-05-31 15:45:12 +0530

    [CAA-21]: added role based GET for orders and status filtering

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -4,11 +4,14 @@ const User = require("../models/user.model");
 const FoodItem = require("../models/food-item.model");
 const authService = require('../services/auth.service');
 const NotFoundException = require("../common/exceptions/NotFoundException");
+const orderStatus = require('../models/orderStatus');
+const roles = require('../models/roles');
 
 exports.create_order = async (req, res, next) => {
   const userId = req.user.id;
 
-  const { type, noOfItems, totalPrice, location, status, foodItems } = req.body;
+  const { type, noOfItems, totalPrice, location, foodItems } = req.body;
+  const status = orderStatus.PENDING; // default status of an order when creating
 
   // checking if fooditem ids are valid
   for(const { id } of foodItems) {
@@ -24,7 +27,7 @@ exports.create_order = async (req, res, next) => {
 
   try {
     // populating from user side
-    let order = await User.relatedQuery('orders').for(userId).insert({ type, noOfItems, totalPrice, location, status });
+    const order = await User.relatedQuery('orders').for(userId).insert({ type, noOfItems, totalPrice, location, status });
     // making a relation from food item side
     for(const { id, quantity } of foodItems) {
       await FoodItem.relatedQuery('orders').for(id).relate({id: order.id, quantity});
@@ -41,16 +44,66 @@ exports.create_order = async (req, res, next) => {
 };
 
 exports.get_all_orders = async (req, res, next) => {
-  Order.query().withGraphFetched('[foodItems]')
-    .then((result) => {
-      res.status(200).json({
-        count: result.length,
-        data: result,
+  const status = req.query.status;
+  const { id, role } = req.user;
+  
+  if(role === roles.CUSTOMER) {
+    if(status) {
+      Order.query()
+        .where('userId', '=', id)
+        .where('status', '=', status)
+        .withGraphFetched('[foodItems]')
+        .then((result) => {
+          res.status(200).json({
+            count: result.length,
+            data: result,
+          });
+        })
+        .catch((error) => {
+          next(error);
+        });
+    } else {
+      Order.query().where('userId', '=', id)
+        .withGraphFetched('[foodItems]')
+        .then((result) => {
+          res.status(200).json({
+            count: result.length,
+            data: result,
+          });
+        })
+        .catch((error) => {
+          next(error);
+        });
+    }
+  } else if (role === roles.MANAGER || role === roles.OWNER) {
+    if(status) {
+      Order.query()
+        .where('status', '=', status)
+        .withGraphFetched('[foodItems]')
+        .then((result) => {
+          res.status(200).json({
+            count: result.length,
+            data: result,
+          });
+        })
+        .catch((error) => {
+          next(error);
+        });
+    } else {
+      Order.query()
+      .withGraphFetched('[foodItems]')
+      .then((result) => {
+        res.status(200).json({
+          count: result.length,
+          data: result,
+        });
+      })
+      .catch((error) => {
+        next(error);
       });
-    })
-    .catch((error) => {
-      next(error);
-    });
+    }
+  }
+
 };
 
 exports.update_order_status = async (req, res, next) => {
---
+++ src/models/orderStatus.js
@@ -0,0 +1,8 @@
+const orderStatus = Object.freeze({
+  PENDING: 'pending',
+  ACCEPTED: 'accepted',
+  DELIVERED: 'delivered',
+  CANCELLED: 'cancelled',
+});
+
+module.exports = orderStatus;
