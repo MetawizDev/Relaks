7f1b12ce912611e036240b4d58d479d94900ec5d
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-06-07 13:56:02 +0530

    [CAA-10]: modified migrations

--- src/database/migrations/20220525163609_promotion-table.js
+++ src/database/migrations/20220525163609_promotion-table.js
@@ -4,7 +4,8 @@ exports.up = function (knex) {
   return knex.schema.createTable(tableName, (table) => {
     table.increments("id").primary();
     table.string("description").notNullable();
-    table.string("order_type", 25).notNullable();
+    table.boolean("is_delivery").notNullable();
+    table.decimal("discount").notNullable();
     table.date("expiry_date");
 
     // timestamps
--- src/database/migrations/20220525163856_promotion-has-food-item-has-portion-table.js
+++ src/database/migrations/20220525163856_promotion-has-food-item-has-portion-table.js
@@ -1,10 +1,12 @@
-const tableName = "promotion_has_food_item";
+const tableName = "promotion_has_food_item_has_portion";
 
 exports.up = function (knex) {
   return knex.schema.createTable(tableName, (table) => {
     table.increments("id").primary();
+    table.integer("quantity").unsigned().notNullable();
     table.integer("food_item_id").unsigned().notNullable().references("id").inTable("food_item");
     table.integer("promotion_id").unsigned().notNullable().references("id").inTable("promotion");
+    table.integer("portion_id").unsigned().notNullable().references("id").inTable("portion");
 
     // timestamps
     table.timestamps(true, true);
