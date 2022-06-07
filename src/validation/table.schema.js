481f920de83084c9ec42c385eb61eb4ce95ea263
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-06-07 09:31:34 +0530

    [CAA-11]: added docs and added role based acl for tables

---
+++ src/validation/table.schema.js
@@ -0,0 +1,17 @@
+const Joi = require("joi");
+
+const Schema = {
+  postTable: Joi.object({
+    tableNo: Joi.number().required(),
+    seatingCapacity: Joi.number().required(),
+  }),
+  patchTable: Joi.object({
+    tableNo: Joi.number(),
+    seatingCapacity: Joi.number(),
+  }),
+  reserveTable: Joi.object({
+    id: Joi.number().required(),
+  }),
+};
+
+module.exports = Schema;
