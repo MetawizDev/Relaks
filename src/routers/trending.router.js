fa0ca917b5fc4a1459e8b79dfb4f3feef8bb2b4c
kavin-du <ukdkcm@gmail.com>
2022-07-08 14:34:38 +0530

    CAA-48: updated swagger docs for trending

--- src/routers/trending.router.js
+++ src/routers/trending.router.js
@@ -5,6 +5,28 @@ router.get("/", trendingController.get_trendings);
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *      TrendingFoodItem:
+ *        type: object
+ *        required:
+ *          - id
+ *          - name
+ *          - categoryId
+ *          - imgUrl
+ *        properties:
+ *          id:
+ *            type: integer
+ *          name:
+ *            type: string
+ *          categoryId:
+ *            type: integer
+ *          imgUrl:
+ *            type: string
+ */
+
+/**
+ * @swagger
  * /api/v1/trending:
  *  get:
  *    summary: Get trending food items
@@ -25,9 +47,8 @@ router.get("/", trendingController.get_trendings);
  *                          count:
  *                            type: integer
  *                            example: 2
- *                          foodItemId:
- *                            type: integer
- *                            example: 4 
+ *                          foodItem:
+ *                            $ref: '#/components/schemas/TrendingFoodItem'
  */
 
 module.exports = router;
