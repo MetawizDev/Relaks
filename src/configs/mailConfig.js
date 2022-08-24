07cede3a50ca7ddfd0150fe502febab9c3044fe3
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-08-24 19:17:43 +0530

    Changed mail server to gmail"

--- src/configs/mailConfig.js
+++ src/configs/mailConfig.js
@@ -1,27 +1,26 @@
-const nodemailer = require('nodemailer');
-
+const nodemailer = require("nodemailer");
 
 exports.sendMail = async (subject, text, to) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    service: "Gmail",
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.MAILPASSWORD,
+    },
+  });
+
   // const transporter = nodemailer.createTransport({
-  //   host: 'smtp.gmail.com',
-  //   service: 'Gmail',
+  //   host: 'smtp.ionos.fr',
+  //   port: 25,
   //   auth: {
   //     user: process.env.MAIL,
   //     pass: process.env.MAILPASSWORD
   //   }
   // });
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ionos.fr',
-    port: 25,
-    auth: {
-      user: process.env.MAIL,
-      pass: process.env.MAILPASSWORD
-    }
-  });
-
   const msgOptions = { subject, text, to, from: process.env.MAIL };
   const result = await transporter.sendMail(msgOptions);
 
   console.log(result);
-}
+};
