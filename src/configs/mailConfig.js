bdf19b66a9b0f3cdc442dc70ffadb7377dab3b26
Buddhi Perera <gkbhperera.tck@gmail.com>
2022-08-24 20:13:42 +0530

    changed email port and host to ionos

--- src/configs/mailConfig.js
+++ src/configs/mailConfig.js
@@ -1,23 +1,23 @@
 const nodemailer = require("nodemailer");
 
 exports.sendMail = async (subject, text, to) => {
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   service: "Gmail",
+  //   auth: {
+  //     user: process.env.MAIL,
+  //     pass: process.env.MAILPASSWORD,
+  //   },
+  // });
+
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    service: "Gmail",
+    host: "smtp.ionos.co.uk",
+    port: 587,
     auth: {
       user: process.env.MAIL,
       pass: process.env.MAILPASSWORD,
     },
   });
-
-  // const transporter = nodemailer.createTransport({
-  //   host: 'smtp.ionos.fr',
-  //   port: 25,
-  //   auth: {
-  //     user: process.env.MAIL,
-  //     pass: process.env.MAILPASSWORD
-  //   }
-  // });
 
   const msgOptions = { subject, text, to, from: process.env.MAIL };
   const result = await transporter.sendMail(msgOptions);
